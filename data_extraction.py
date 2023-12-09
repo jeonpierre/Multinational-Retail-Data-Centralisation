8de5953f475397a9f1b702d57302fe224810177b
Jean-Pierre Viljoen <jp@Jean-Pierres-MacBook-Air.local>
2023-12-09 20:10:24 +0000

    Add data extraction and cleaning classes

---
+++ data_extraction.py
@@ -0,0 +1,10 @@
+class DataExtractor:
+    def extract_csv_data(self, file_path)
+        
+    def extract_api_data(self, api_url)
+        
+    def extract_s3_bucket_data(self, bucket_name, prefix)
+        
+
+
+
