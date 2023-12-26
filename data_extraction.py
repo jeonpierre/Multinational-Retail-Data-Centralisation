8a9096304e8d76aa6d691eb2ebb4a857d477dfc4
Jean-Pierre Viljoen <jp@jeanpierresair.home>
2023-12-26 15:26:00 +0000

    completed milestone 2

--- data_extraction.py
+++ data_extraction.py
@@ -1,47 +1,67 @@
-import yaml 
+
+from database_utils import DatabaseConnector
 import sqlalchemy
-import psycopg2
 import pandas as pd
-from sqlalchemy import inspect
+import tabula
+import requests
+import boto3 
 
-with open('db_creds.yaml', 'r') as f:
-    db_creds = yaml.safe_load(f)
 
-class DataExtractor:
-    def read_db_creds(self, credential):
-       with open("db_creds.yaml", 'r') as f:
-        db_creds = yaml.safe_load(f)
-        return db_creds
+class DataExtractor():
 
-        return db_creds
+    def __init__(self):
+        self.db = DatabaseConnector()
+        self.rds_database = self.db.init_db_engine()
+        self.api_key ={'x-api-key':'yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX'}
 
-    def init_db_engine(self): # 
-        db_creds = self.read_db_creds("db_creds.yaml")
-        from sqlalchemy import create_engine 
-        engine = create_engine(f"postgresql://{db_creds['RDS_USER']}:{db_creds['RDS_PASSWORD']}@{db_creds['RDS_HOST']}:{db_creds['RDS_PORT']}/{db_creds['RDS_DATABASE']}")
-        return engine
+
+    def read_rds_table(self, table_name):
+      
+      return pd.read_sql_table(table_name, self.rds_database)
     
-    def list_db_tables(self, engine):
-       inspector = inspect(engine)
-       tables = inspector.get_table_names()
-       return tables
+
+    def retrieve_pdf_data(self, link):
+        link = 'https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf'
+        pdf_dataframes = tabula.read_pdf(link, pages="all", multiple_tables=True)
+
+        return pd.concat(pdf_dataframes,ignore_index=True)
     
-   #def read_rds_table(self, engine)'
-        
+
+    def list_number_of_store(self):
+        stores = requests.get('https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores',headers=self.api_key)
+        number_of_stores = stores.json()
+        return number_of_stores["number_stores"]
     
-data_extractor = DataExtractor()
-engine = data_extractor.init_db_engine()
-tables = data_extractor.list_db_tables(engine)
-print(tables)
 
-'''tables = engine.execute("SELECT table_name FROM information_schema.tables").fetchall()
-        tables = [table[0] for table in tables]
-        print(tables)
-        return tables
-        result = connection.execute("SELECT...") '''
+    def retrieve_stores_data(self):
+        number_of_stores = self.list_number_of_store()
+        stores_list = []
+        for store_number in range(0, number_of_stores):
+            stores_list.append(requests.get('https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/'+str(store_number), headers=self.api_key).json())
+        return pd.json_normalize(stores_list)
+    
 
+    def extract_from_s3(self,address):
+        s3 = boto3.client('s3')
+        bucket = 'data-handling-public'
+        object = 'products.csv'
+        file = 'products.csv'
+        s3.download_file(bucket,object,file)
+        table = pd.read_csv('./products.csv')
+        return table
+    
 
-'''with engine.connect() as connection:
-            result = connection.execute("SELECT table_name FROM information_schema.tables")
-            tables = [row[0] for row in result]
-        return tables'''
+    def extract_from_s3_json(self):
+        #https://data-handling-public.s3.eu-west-1.amazonaws.com/date_details.json
+        s3 = boto3.client('s3')
+        bucket = 'data-handling-public'
+        object = 'date_details.json'
+        file = 'date_details.json'
+        s3.download_file(bucket,object,file)
+        table = pd.read_json('./date_details.json')
+        # print(table)
+        return table
+    
+
+if __name__ == "__main__":
+    isinstance = DataExtractor()
--- database_utils.py
+++ database_utils.py
@@ -1,49 +1,83 @@
-import yaml 
+import yaml
 import sqlalchemy
 import psycopg2
-import pandas as pd
-from sqlalchemy import inspect
 
-with open('db_creds.yaml', 'r') as f:
-    db_creds = yaml.safe_load(f)
 
-class DatabaseConnector:
-    def read_db_creds(self, credential):
-       with open("db_creds.yaml", 'r') as f:
-        db_creds = yaml.safe_load(f)
-        return db_creds
+HOST = 'localhost'
+USER = 'postgres'
+PASSWORD = '80684123'
+DATABASE = 'sales_data'
+PORT = 5432
+'sales_data_connection = psycopg2.(host=HOST, user=USER, password=PASSWORD, dbname=DATABASE, port=PORT)'
+conn = psycopg2.connect(host=HOST, user=USER, password=PASSWORD, dbname=DATABASE, port=PORT) # Establish connection
 
-        return db_creds
+conn.set_session(readonly=False)
 
-    def init_db_engine(self): # 
-        db_creds = self.read_db_creds("db_creds.yaml")
-        from sqlalchemy import create_engine 
-        engine = create_engine(f"postgresql://{db_creds['RDS_USER']}:{db_creds['RDS_PASSWORD']}@{db_creds['RDS_HOST']}:{db_creds['RDS_PORT']}/{db_creds['RDS_DATABASE']}")
+# Check if connection is read-only
+if conn.readonly:
+    print("Connection is in read-only mode. Changing isolation level.")
+    conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_READ_COMMITTED)
+
+
+import psycopg2
+
+class DatabaseConnector():
+
+    def read_db_creds(self):
+        with open('db_creds.yaml', 'r') as file:
+            creds = yaml.safe_load(file)
+        return creds
+    
+    def init_db_engine(self):
+        db_creds = self.read_db_creds()
+      
+        engine = sqlalchemy.create_engine(f"postgresql://{db_creds['RDS_USER']}:{db_creds['RDS_PASSWORD']}@{db_creds['RDS_HOST']}:{db_creds['RDS_PORT']}/{db_creds['RDS_DATABASE']}")
+        engine.execution_options(isolation_level='AUTOCOMMIT').connect()
         return engine
-    
-    def list_db_tables(self, engine):
-       inspector = inspect(engine)
-       tables = inspector.get_table_names()
-       return tables
-    
-    'def read_rds_table(self, engine):'
+
+    def list_db_tables(self):
+        engine = self.init_db_engine()
+        with engine.connect() as connection:
+            # Get the metadata of the database
+            metadata = sqlalchemy.MetaData()
+            metadata.reflect(bind=connection)
+
+            # Get the table names from the metadata
+            table_names = metadata.tables.keys()
+
+            return table_names
         
+
+    def upload_to_db(self, df, table_name):
+        DATABASE_TYPE = 'postgresql'
+        DBAPI = 'psycopg2'
+        HOST = 'localhost'
+        USER = 'postgres'
+        PASSWORD = 'confidential'
+        DATABASE = 'sales_data'
+        PORT = 5432
+        local_engine = sqlalchemy.create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}")
+        df.to_sql(table_name, local_engine, if_exists='replace')
+
+
+if __name__ == '__main__':
+    print('Tables in Database:', DatabaseConnector().list_db_tables())
+    from data_cleaning import DataCleaning
+    from data_extractor import DataExtractor
+    import jpype
+    import os
+   # print(os.environ['JAVA_HOME']) 
+    #print(os.listdir(os.environ['JAVA_HOME']))
+    #print(jpype.getDefaultJVMPath())
+    #DatabaseConnector().upload_to_db(DataCleaning().clean_user_data(),'dim_users')    
+    #DatabaseConnector().upload_to_db(DataCleaning().clean_card_data(), 'dim_card_details')  
+    # DatabaseConnector().upload_to_db(DataCleaning().clean_store_data(), 'dim_store_details')
+
+    # Retrieve data from S3 Clean & Upload dim_products
+    '''products_df = DataExtractor().extract_from_s3('products.csv')  # Retrieve data from S3 Clean & Upload dim_products
+    products_df = DataCleaning().clean_products_data(products_df) 
+    DatabaseConnector().upload_to_db(products_df, 'dim_products')'''
+
+    # DatabaseConnector().upload_to_db(DataCleaning().clean_orders_data(), 'orders_table') # Clean Orders Table Data to a Table on Database
     
-data_connector = DatabaseConnector()
-engine = data_connector.init_db_engine()
-tables = data_connector.list_db_tables(engine)
-print(tables)
-
-'''tables = engine.execute("SELECT table_name FROM information_schema.tables").fetchall()
-        tables = [table[0] for table in tables]
-        print(tables)
-        return tables
-        result = connection.execute("SELECT...") '''
-
-
-'''with engine.connect() as connection:
-            result = connection.execute("SELECT table_name FROM information_schema.tables")
-            tables = [row[0] for row in result]
-        return tables'''
-
-
+    # DatabaseConnector().upload_to_db(DataCleaning().clean_date_times(), 'dim_date_times') # Clean & Upload Date times 
